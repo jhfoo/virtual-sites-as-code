e2ea5a0db26f370b157afe85244a895cbf1eb290
Foo JH <foojihaw@hotmail.com>
2022-02-13 00:04:36 -0600

    NEW: Download master config from GH private repo

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # virtual-sites-as-code
 Generate standard virtual sites from yaml files
+
+## Execute
+```
+npx mastermind-hostascode user/reponame/filename.yaml
+```
+## Features
+- Download master config from GitHub private repo
+
+## Notes
+### GitHub API token
+2 options to pass token:
+- Pass as second CLI argument: 
+```
+npx mastermind-hostascode jhfoo/reponame/config.yaml <api token>
+```
+- Add line to ~/.mastermind: 
+```
+github_token=<api token>
+```
--- bin/mastermind.js
+++ bin/mastermind.js
@@ -1,5 +1,10 @@
 #! /usr/bin/env node
-const process = require('process')
+const { mainModule } = require('process')
+const fs = require('fs'),
+  path = require('path'),
+  process = require('process'),
+  jsyaml = require('js-yaml'),
+  axios = require('axios')
 
 function validateCli() {
   if (process.argv.length < 3) {
@@ -7,4 +12,56 @@ function validateCli() {
   }
 }
 
+function getTokenFromDotFile() {
+  const FullPath = process.env.HOME + '/.mastermind'
+  console.log(`FullPath: ${FullPath}`)
+  if (fs.existsSync(FullPath)) {
+    const lines = fs.readFileSync(FullPath, 'utf8').split('\n')
+    const FileVars = lines.reduce((final, line) => {
+      const matches = line.match(/^(.+?)=(.+)$/)
+      if (matches) {
+        final[matches[1]] = matches[2]
+      }
+      return final
+    }, {})
+
+    if ('token_github' in FileVars) {
+      return FileVars.token_github
+    }
+  } else {
+    throw new Error('Missing API token')
+  }
+}
+
+function validateConfig(sites) {
+  if (!('webserver' in sites)) {
+    throw new Error('Missing webserver')
+  }
+  if (!('path' in sites.webserver)) {
+    throw new Error('Missing webserver.path')
+  }
+
+}
+
+async function main() {
+  const GITHUB_FILE_PATH = process.argv[2]
+  console.log(`File: ${GITHUB_FILE_PATH}`)
+  try {
+    const GITHUB_TOKEN = process.argv.length > 3 ? process.argv[3] : getTokenFromDotFile()
+    const resp = await axios.get(GITHUB_FILE_PATH, {
+      headers: {
+        Accept: 'application/vnd.github.v3.raw',
+        Authorization: `token ${GITHUB_TOKEN}`,
+      }
+    })
+    const VirtualSites = jsyaml.load(resp.data)
+
+    validateConfig(VirtualSites)
+    console.log(VirtualSites)
+  } catch (err) {
+    console.error(err.message)
+  }
+}
+
 validateCli()
+main()
